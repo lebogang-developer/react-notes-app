96024a8ea63fec8396fed2ca13938195c19bcfa8
Lebogang Sekaleli <lebogangsekaleli@gmail.com>
2026-03-26 12:58:06 +0200

    Form data object

--- src/components/NoteForm.jsx
+++ src/components/NoteForm.jsx
@@ -1,61 +1,86 @@
-import {useState} from 'react'
+import { useState } from "react";
 
 const NoteForm = () => {
-    const [title, setTitle] = useState('');
-    const [priority, setPriority] = useState('Medium');
-    const [category, setCategory] = useState('Work');
-    const [description, setDescription] = useState('');
+  const [formData, setFormData] = useState({
+    title: "",
+    priority: "High",
+    category: "Work",
+    description: "",
+  });
 
-    return ( <form className='mb-6'>
-        <div className='mb-4'>
-            <label htmlFor='title' className='block font-semibold'>Title</label>
-            <input
-            type='text'
-            className='w-full p-2 border rounded-lg'
-            value={title}
-            onChange={(e)=> seTitle(e.target.value)}
-            />
-        </div>
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-        <div className='mb-4'>
-            <label htmlFor='priority' className='block font-semibold'>Priority</label>
-            <select
-            type='text'
-            className='w-full p-2 border rounded-lg'
-            value={priority}
-            onChange={(e)=> setPriority(e.target.value)}
-            >
-            <option value='High'>High</option>
-            <option value='High'>Medium</option>
-            <option value='Low'>Low</option>
-            </select>
-        </div>
+  return (
+    <form className="mb-6">
+      <div className="mb-4">
+        <label htmlFor="title" className="block font-semibold">
+          Title
+        </label>
+        <input
+          name="title"
+          type="text"
+          className="w-full p-2 border rounded-lg"
+          value={formData.title}
+          onChange={handleChange}
+        />
+      </div>
 
-        <div className='mb-4'>
-            <label htmlFor='category' className='block font-semibold'>Category</label>
-            <select
-            type='text'
-            className='w-full p-2 border rounded-lg'
-            value={category}
-            onChange={(e)=> setCategory(e.target.value)}
-            >
-            <option value='Work'>Work</option>
-            <option value='Personal'>Personal</option>
-            <option value='Ideas'>Ideas</option>
-            </select>
-        </div>
+      <div className="mb-4">
+        <label htmlFor="priority" className="block font-semibold">
+          Priority
+        </label>
+        <select
+          name="priority"
+          type="text"
+          className="w-full p-2 border rounded-lg"
+          value={formData.priority}
+          onChange={handleChange}
+        >
+          <option value="High">High</option>
+          <option value="High">Medium</option>
+          <option value="Low">Low</option>
+        </select>
+      </div>
 
-         <div className='mb-4'>
-            <label htmlFor='description' className='block font-semibold'>Description</label>
-            <textarea
-            type='text'
-            className='w-full p-2 border rounded-lg'
-            value={description}
-            onChange={(e)=> setDescription(e.target.value)}>
-            </textarea>
-        </div>
-        <button className='w-full bg-purple-500 text-white py-2 rounded-lg cursor-pointer hover:bg-purple-600'>Add Note</button>
-    </form> );
-}
- 
+      <div className="mb-4">
+        <label htmlFor="category" className="block font-semibold">
+          Category
+        </label>
+        <select
+          name="category"
+          type="text"
+          className="w-full p-2 border rounded-lg"
+          value={formData.category}
+          onChange={handleChange}
+        >
+          <option value="Work">Work</option>
+          <option value="Personal">Personal</option>
+          <option value="Ideas">Ideas</option>
+        </select>
+      </div>
+
+      <div className="mb-4">
+        <label htmlFor="description" className="block font-semibold">
+          Description
+        </label>
+        <textarea
+          name="description"
+          type="text"
+          className="w-full p-2 border rounded-lg"
+          value={formData.description}
+          onChange={handleChange}
+        ></textarea>
+      </div>
+      <button className="w-full bg-purple-500 text-white py-2 rounded-lg cursor-pointer hover:bg-purple-600">
+        Add Note
+      </button>
+    </form>
+  );
+};
+
 export default NoteForm;
